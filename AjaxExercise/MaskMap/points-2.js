a667aa1382932b552f65a0ff4f9d3c2db6fa5eed
Sharon Chen <treetwood@gmail.com>
2020-08-03 09:44:48 +0800

    fixed image problem

---
+++ AjaxExercise/MaskMap/points-2.js
@@ -0,0 +1,109 @@
+loadData({
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "properties": {
+            "id": "5938030253",
+            "name": "健泰藥局",
+            "phone": "049-2336280",
+            "address": "南投縣草屯鎮碧峰里碧山路１０８２號",
+            "mask_adult": 1691,
+            "mask_child": 0,
+            "updated": "2020\/07\/31 14:44:37",
+            "available": "星期一上午看診、星期二上午看診、星期三上午看診、星期四上午看診、星期五上午看診、星期六上午看診、星期日上午休診、星期一下午看診、星期二下午看診、星期三下午看診、星期四下午看診、星期五下午看診、星期六下午看診、星期日下午休診、星期一晚上看診、星期二晚上看診、星期三晚上看診、星期四晚上看診、星期五晚上看診、星期六晚上看診、星期日晚上休診",
+            "note": "平日8:30現場販售，售完為止。星期日公休",
+            "custom_note": "",
+            "website": "",
+            "county": "南投縣",
+            "town": "草屯鎮",
+            "cunli": "碧峰里",
+            "service_periods": "NNNNNNYNNNNNNYNNNNNNY"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                120.657423,
+                23.97377
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "id": "5941271455",
+            "name": "成吉藥局仁義店",
+            "phone": "(06)2497307",
+            "address": "臺南市仁德區仁義路４２之１號",
+            "mask_adult": 2963,
+            "mask_child": 313,
+            "updated": "2020\/07\/31 14:44:37",
+            "available": "",
+            "note": "販售口罩時段為每星期一至星期日上午9點至12點",
+            "custom_note": "",
+            "website": "",
+            "county": "臺南市",
+            "town": "仁德區",
+            "cunli": "仁義里",
+            "service_periods": ""
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                120.253913,
+                22.968081
+            ]
+        }
+    },{
+        "type": "Feature",
+        "properties": {
+            "id": "5939091303",
+            "name": "莿桐台丞藥局",
+            "phone": "(05)5840808",
+            "address": "雲林縣莿桐鄉中正路１６６號",
+            "mask_adult": 1071,
+            "mask_child": 420,
+            "updated": "2020\/07\/31 14:44:37",
+            "available": "",
+            "note": "販售口罩時段為每星期一至星期日上午9點至12點",
+            "custom_note": "",
+            "website": "",
+            "county": "雲林縣",
+            "town": "莿桐鄉",
+            "cunli": "莿桐村",
+            "service_periods": ""
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                120.49911,
+                23.757509
+            ]
+        }
+    },{
+        "type": "Feature",
+        "properties": {
+            "id": "2336210034",
+            "name": "臺中市和平區梨山衛生所",
+            "phone": "04-25989540",
+            "address": "臺中市和平區梨山里中正路６８號",
+            "mask_adult": 0,
+            "mask_child": 0,
+            "updated": "2020\/07\/31 14:44:37",
+            "available": "星期一上午看診、星期二上午看診、星期三上午看診、星期四上午看診、星期五上午看診、星期六上午看診、星期日上午看診、星期一下午看診、星期二下午看診、星期三下午看診、星期四下午看診、星期五下午看診、星期六下午看診、星期日下午看診、星期一晚上看診、星期二晚上看診、星期三晚上看診、星期四晚上看診、星期五晚上看診、星期六晚上看診、星期日晚上看診",
+            "note": "提供24小時急診待診服務，109年6月1日起，口罩販售時段為周一至周五0900-12:00，周六、日09:00-11:00",
+            "custom_note": "",
+            "website": "",
+            "county": "臺中市",
+            "town": "和平區",
+            "cunli": "梨山里",
+            "service_periods": "NNNNNNNNNNNNNNNNNNNNN"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                121.255139,
+                24.254349
+            ]
+        }
+    },]
+})
